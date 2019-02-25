6b6cb15e4d297c2ce37f4e63d3ffcae80d75f853
Damien Brun <makowildcat@gmail.com>
2019-02-24 22:21:14 -0500

    Create Projectile script to follow (look at and move toward) a target

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour 
+{
+
+	public Transform target;
+
+	public float speed;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.LookAt(target);
+		Vector3 direction = target.position - transform.position;
+		transform.Translate(direction * speed * Time.deltaTime, Space.World);
+	}
+}
