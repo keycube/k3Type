e4a02e315cb6ec89302de68f223964daf2fb8936
Damien Brun <makowildcat@gmail.com>
2019-02-26 00:21:49 -0500

    Create waves of Enemy

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,15 +6,28 @@ public class GameController : MonoBehaviour
 {
 	public GameObject enemyPrefab;
 	public Vector3 spawnValues;
+	public int enemyCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
 
 	void Start () 
 	{
-		SpawnWaves();
+		StartCoroutine(SpawnWaves());
 	}
 
-	private void SpawnWaves()
+	IEnumerator SpawnWaves()
 	{
-		Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
-		Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+		yield return new WaitForSeconds(startWait);
+		while(true) 
+		{
+			for (int i = 0; i < enemyCount; i++)
+			{
+				Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+				yield return new WaitForSeconds(spawnWait);
+			}
+			yield return new WaitForSeconds(waveWait);
+		}
 	}
 }
