19febbcd2a19ad9b2d82cff22cb471dc187c4ed6
Damien Brun <makowildcat@gmail.com>
2019-02-25 23:42:11 -0500

    Create GameController to spawn Enemy (prefab)

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour 
+{
+	public GameObject enemyPrefab;
+	public Vector3 spawnValues;
+
+	void Start () 
+	{
+		SpawnWaves();
+	}
+
+	private void SpawnWaves()
+	{
+		Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+		Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+	}
+}
